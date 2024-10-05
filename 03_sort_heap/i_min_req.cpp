4a5b8586382977486f4046f5941373a12875e75d
wolches <anton.wolches@gmail.com>
2024-10-05 20:40:01 +0300

    sort_heap: i partially done

--- 03_sort_heap/i_min_req.cpp
+++ 03_sort_heap/i_min_req.cpp
@@ -1,26 +1,52 @@
 #include <bits/stdc++.h>
+#define uint uint32_t
 
 using namespace std;
 
+const uint m232 = 0xffffffff;
 int x0, x1, a, b, c;
 
-uint32_t next() {
+uint next() {
     uint32_t t = x0 * a + x1 * b + c;
     x0 = x1;
     x1 = t;
     return x0 >> 2;
 }
 
+void sift_up(vector<uint>& array, uint i) {
+    while (i > 1 && array[i] < array[i / 2]) {
+        swap(array[i], array[i / 2]);
+        i /= 2;
+    }
+}
+
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    int n, q;
+    uint n, q;
     cin >> n >> q >> x0 >> x1 >> a >> b >> c;
 
+    vector<uint> array(n + 1);
+    for (int i = 0; i < n + 1; ++i) {
+        array[i] = m232;
+    }
 
+    uint res, sum = 0, multiply = 10099;
 
+    while (q--) {
+        uint i = next();
+        uint x = next();
+        array[i % n + 1] = x;
+        //cout << "a[" << i % n << "] = " << x << '\n';
+        sift_up(array, i % n + 1);
+        //cout << "min = " << array[1] << '\n';
 
-    vector<int> nums(n);
+        res = array[1] * multiply;
 
+        sum += res;
+        multiply *= 10099;
+    }
+
+    cout << sum;
 }
---
+++ 03_sort_heap/test.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+#define uint uint32_t
+
+using namespace std;
+
+int main() {
+
+     unsigned int a = 0xffffffff;
+     cout << ++a;
+}
