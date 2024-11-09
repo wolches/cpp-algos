1640f5679ba9146be305d693239d61f5d6e0c548
wolches <anton.wolches@gmail.com>
2024-11-09 20:22:52 +0300

    dp: k

--- 06_dynamic_programming/k_tickets_queue.cpp
+++ 06_dynamic_programming/k_tickets_queue.cpp
@@ -3,8 +3,7 @@
 using namespace std;
 
 struct p {
-    int a, b, c, dp;
-    char type = 'a';
+    int a, b, c;
 };
 
 int main() {
@@ -19,27 +18,22 @@ int main() {
         cin >> p[i].a >> p[i].b >> p[i].c;
     }
 
+    vector<int> dp(n);
+
+    int a, b, c;
     for (int i = 0; i < n; ++i) {
-        p[i].dp = p[i].a;
-        if (i > 0) {
-            if (p[i - 1].b < (p[i - 1].dp + p[i].dp) && p[i - 1].dp > 0) {
-                p[i - 1].dp = p[i - 1].b;
-                p[i].dp = 0;
-            }
-        }
+        a = p[i].a + (i == 0 ? 0 : dp[i - 1]);
         if (i > 1) {
-            if (p[i - 2].c < (p[i - 2].dp + p[i - 1].dp + p[i].dp) && p[i - 2].dp > 0) {
-                p[i - 2].dp = p[i - 2].c;
-                p[i - 1].dp = 1;
-                p[i].dp = 0;
-            }
+           b = p[i - 1].b + dp[i - 2];
+           c = p[i - 2].c + (i == 2 ? 0 : dp[i - 3]);
+           dp[i] = min(min(a, b), c);
+        } else if (i > 0) {
+           b = p[i - 1].b + dp[i - 2];
+           dp[i] = min(a, b);
+        } else {
+           dp[i] = a;
         }
     }
 
-    int ans = 0;
-    for (const auto &item: p) {
-        ans += item.dp;
-    }
-
-    cout << ans;
+    cout << dp[n - 1];
 }
